6820bd6f542844b63f16226a2b4f9477e0e68d67
QbasicFan <philSerme@gmail.com>
2018-01-28 17:16:45 -0500

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+
+
+urlpatterns = [
+#test this !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    url(r'^', views.index, name='index'),
+
+            ]
