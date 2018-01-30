af57ef30b3ecfc806b84019463452b44bcb73c38
QbasicFan <philSerme@gmail.com>
2018-01-30 02:33:53 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import  url
+
 from . import views
 
+handler404 = "mezzanine.core.views.page_not_found"
 
 
 urlpatterns = [
 #test this !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     url(r'^', views.index, name='index'),
-
             ]
