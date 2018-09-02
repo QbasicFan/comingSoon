1351563f26430e8b9e8ba6aaf6b7c925986962aa
QbasicFan <philSerme@gmail.com>
2018-09-02 16:53:29 -0400

    Update views.py

--- views.py
+++ views.py
@@ -1,39 +1,38 @@
 from django.shortcuts import render
+
 from comingSoon.forms import eMailForm
 
-from .models import mainImage , comingDate , eMail , background
+from .models import comingDate , eMail , frontErrorPage , SBtn , backgroundImage
+
 # Create your views here.
 
 def index(request):
-   title = mainImage.objects.all()
    cDate = comingDate.objects.all()
    email = eMail.objects.all()
-   bkg = background.objects.all()
-
-
-
-
-   image1 = title.filter(title="title1")
-   image2 = title.filter(title="title2")
-   image3 = title.filter(title="title3")
-
+   bkg = frontErrorPage.objects.all()
+   SB = SBtn.objects.all()
+   bkgImg = backgroundImage.objects.all()
 
 
    if request.method == 'POST':
         form = eMailForm(request.POST)
         if form.is_valid():
             form.save()
-            joe = "thanks you for sharing your informations with us, you will be notified when 888vital will be ready"
-            context = {'jack':joe,"image1":image1,"image2":image2,"image3":image3,"cDate":cDate,"email":email , "bkg":bkg}
-            return render(request, 'vital/index.html', context )
+            joe = "Thank you for contacting us, you will be notified when  888vital is ready!!!!"
+            context = {'jack':joe,"cDate":cDate,"email":email , "bkg":bkg , "SBtn":SB , "bkgImg":bkgImg}
+            return render(request, 'comingSoon/index.html', context )
         else:
-            return render(request, 'vital/error.html', {})
+            return render(request, 'comingSoon/error.html', {})
    else:
         form = eMailForm()
-        context = {'form':form,"image1":image1,"image2":image2,"image3":image3,"cDate":cDate,"email":email , "bkg":bkg}
-   return render(request, 'vital/index.html', context )
+        context = {'form':form,"cDate":cDate,"email":email , "bkg":bkg , "SBtn":SB , "bkgImg":bkgImg}
+   return render(request, 'comingSoon/index.html', context )
+
 
 ##############################################
 
 def handler404(request):
-    return render(request,'vital/error.html', {})
+    bkg = frontErrorPage.objects.all()
+
+    return render(request,'comingSoon/error.html', {"bkg":bkg})
+
